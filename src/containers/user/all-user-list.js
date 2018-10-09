0f1941dff6a257812caa45b1e8ab1b72b7327455
zlh <17612235899@163.com>
2018-10-09 22:25:33 +0800

    添加列

--- src/containers/user/all-user-list.js
+++ src/containers/user/all-user-list.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Table, Icon ,Input,Button} from 'antd';
+import {Table, Icon, Input, Button} from 'antd';
 import Api from '@api/tourongApi';
 import PropTypes from 'prop-types';
-const InputSearch=Input.Search;
+
+const InputSearch = Input.Search;
 import './all-user-list.less'
-export default class AllAllUserList extends React.Component{
+
+export default class AllAllUserList extends React.Component {
 
     // 构造
     constructor(props) {
         super(props);
         // 初始状态
         this.state = {
-            data:[{
-
-            }],
-            disabled:true
+            data: [{}],
+            disabled: true
         };
     }
 
@@ -28,18 +28,18 @@ export default class AllAllUserList extends React.Component{
         title: 'ID',
         dataIndex: 'id',
         key: 'id',
-    },{
+    }, {
         title: '用户类型',
         dataIndex: 'type',
         key: 'type',
-        render:(text)=>{
-            if(text===1){
+        render: (text) => {
+            if (text === 1) {
                 return '资金方'
-            }else if(text===2){
+            } else if (text === 2) {
                 return '资产方'
             }
         }
-    },{
+    }, {
         title: '姓名',
         dataIndex: 'name',
         key: 'name',
@@ -51,19 +51,47 @@ export default class AllAllUserList extends React.Component{
         title: '部门',
         dataIndex: 'department',
         key: 'department',
-    },{
+    }, {
         title: '职位',
         dataIndex: 'position',
         key: 'position',
-    }, {
+    },
+    {
+        title: '审核状态',
+        dataIndex: 'review',
+        key: 'review',
+        render:(text)=>{
+            console.log(text);
+            if(text===0){
+                return '待审核'
+            }else if(text===1){
+                return '已通过'
+            }else{
+                return '已拒绝'
+            }
+        }
+    },
+        {
+            title: '会员状态',
+            dataIndex: 'payOk',
+            key: 'payOk',
+            render:(text)=>{
+                if(text===0){
+                    return '非会员'
+                }else if(text===1){
+                    return '会员'
+                }
+            }
+        },
+    {
         title: '操作',
         key: 'action',
         render: (text, record) => {
 
             return (
                 <span>
-                    <a href="javascript:;" onClick={this.gotoView.bind(this,record.id)}>查看信息</a>
-                </span>
+                <a href="javascript:;" onClick={this.gotoView.bind(this, record.id)}>查看信息</a>
+            </span>
             )
         },
     }];
@@ -72,71 +100,74 @@ export default class AllAllUserList extends React.Component{
     componentWillMount() {
 
         // 获取所有用户，筛选出还未审核的用户
-        Api.getAllUser({
-
-        }).then((data)=>{
-            console.log(111222,data);
+        Api.getAllUser({}).then((data) => {
+            console.log(111222, data);
             let d = data.data;
             let newData = [];
-            for (let i=0;i<d.length;i++){
+            for (let i = 0; i < d.length; i++) {
                 let obj = d[i];
-                    let o = {
-                        key:i,
-                        name:obj.name,
-                        orgName:obj.orgName, //机构名称
-                        department:obj.department, //部门
-                        position:obj.position, //职位
-                        id:obj.id,
-                        type:obj.type,
+                let o = {
+                    key: i,
+                    name: obj.name,
+                    orgName: obj.orgName, //机构名称
+                    department: obj.department, //部门
+                    position: obj.position, //职位
+                    id: obj.id,
+                    type: obj.type,
+                    review:obj.review,
+                    payOk:obj.payOk
 
-                    };
-                    newData.push(o);
+                };
+                newData.push(o);
 
             }
-            this.setState({data:newData});
-            console.log(998,newData);
+            this.setState({data: newData});
+            console.log(998, newData);
         })
     }
 
     // 进入详情页
-    gotoView(userId){
-        this.context.router.history.push('/admin/user/audit/'+userId); // 带参数跳转路由
+    gotoView(userId) {
+        this.context.router.history.push('/admin/user/audit/' + userId); // 带参数跳转路由
     }
-    onSearch=(val)=>{
-        Api.getInfoFormName({name:val}).then(res=>{
-            if(res.status===0){
+
+    onSearch = (val) => {
+        Api.getInfoFormName({name: val}).then(res => {
+            if (res.status === 0) {
                 this.setState({
-                    disabled:false,
-                    data:res.data
+                    disabled: false,
+                    data: res.data
                 })
             }
         })
     };
-    getAllUsers=()=>{
-        Api.getInfoFormName({}).then(res=>{
-            if(res.status===0){
+    getAllUsers = () => {
+        Api.getInfoFormName({}).then(res => {
+            if (res.status === 0) {
                 this.setState({
-                    disabled:true,
-                    data:res.data
+                    disabled: true,
+                    data: res.data
                 })
             }
         })
     };
-    render(){
-        let {disabled}=this.state;
+
+    render() {
+        let {disabled} = this.state;
         return (
             <div className='all-user-list'>
-                <div style={{marginBottom:'10px'}}>
-                    <Button className="getAllUsers" type='primary' disabled={disabled} onClick={this.getAllUsers}>查看全部用户</Button>
+                <div style={{marginBottom: '10px'}}>
+                    <Button className="getAllUsers" type='primary' disabled={disabled}
+                            onClick={this.getAllUsers}>查看全部用户</Button>
                     <InputSearch
                         placeholder="请输入姓名或公司名称搜索"
-                        style={{ width: 200 }}
+                        style={{width: 200}}
                         onSearch={this.onSearch}
                     />
 
                 </div>
 
-                <Table columns={this.columns} dataSource={this.state.data} />
+                <Table columns={this.columns} dataSource={this.state.data}/>
             </div>
         )
     }
